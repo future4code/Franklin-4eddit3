d163e6ab5c13c6f13f207d873d9f80d861e0967d
Danyelleac <danyelleacandido@gmail.com>
2022-07-27 13:52:28 -0300

    feed-page- falta a post input card

--- LabEddit/src/components/PostCard/PostCard.js
+++ LabEddit/src/components/PostCard/PostCard.js
@@ -3,14 +3,15 @@ import { PostCardContainer, StyleVote } from "./styled";
 const PostCard = (props) => {
 
     return (
-        <PostCardContainer>
-            <p>Enviado por: @Incididunt</p>
-            <p>Occaecat duis dolor id ullamco officia dolore labore nisi deserunt adipisicing.</p>
+        <PostCardContainer onClick={props.onClick}>
+            <p>Enviado por: @{props.userName}</p>
+            <p>{props.body}</p>
             <StyleVote>
                 <span class="material-icons">arrow_upward</span>
-                <p>345</p>
+                <p>{props.voteSum}</p>
                 <span class="material-icons">arrow_downward</span>
-                {props.resposta === true ? <span></span> : <span class="material-icons">chat_bubble_outline</span>}
+                {props.resposta === true ? <span></span> : <span class="material-icons">chat_bubble_outline</span>} 
+                <p>{props.commentCount}</p>
             </StyleVote>
         </PostCardContainer>
     )
--- LabEddit/src/pages/FeedPage/FeedPage.js
+++ LabEddit/src/pages/FeedPage/FeedPage.js
@@ -1,15 +1,40 @@
+import React from "react";
 import { Header } from "../../components/Header/Header";
 import { useAppNavigate } from "../../routes/coordinator";
+import useRequestData from "../../hooks/useRequestData";
+import { BASE_URL } from "../../constants/urls";
+import useProtectedPage from "../../hooks/useProtectedPage"; 
+import PostCard from "../../components/PostCard/PostCard";
 
 const FeedPage = () => {
+
+    useProtectedPage()
     const { goToPostPage } = useAppNavigate();
-    return (
-        <>
-            <Header/>
-            <h1>FeedPage</h1>
-            <button onClick={() => { goToPostPage() }} >Post Page</button>
-        </>
-    );
+    const posts = useRequestData([], `${BASE_URL}/posts`)
+    const onClickCard = (id) =>{
+        goToPostPage(id)
+   }
+
+    const postsCards = posts.map((post) => {
+        return (
+            <PostCard
+            onClick={()=> onClickCard(post.id)}
+            key={post.id}
+            body={post.body}
+            userName={post.username}
+            voteSum={post.voteSum}
+            commentCount={post.commentCount}
+/>
+        )})
+
+    
+  return (
+    <div>
+        <Header/>
+        {postsCards}
+      <button onClick={() => goToPostPage()}>Post Page </button>
+    </div>
+  )
 }
 
 export default FeedPage;
