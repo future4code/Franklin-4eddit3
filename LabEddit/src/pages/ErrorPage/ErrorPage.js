8f206cf90c2335717ba6dfa862ea5aa1ca9368e3
danyelleac <danyelleacandido@gmail.com>
2022-07-29 10:11:15 -0300

    error page

--- LabEddit/src/pages/ErrorPage/ErrorPage.js
+++ LabEddit/src/pages/ErrorPage/ErrorPage.js
@@ -1,12 +1,21 @@
 import { useAppNavigate } from "../../routes/coordinator";
+import Logo from "../../assets/logo.svg";
+import { ErrorPageContiner, StyleTitle ,StyleText, StyleButton, LogoImg} from "./styled";
+import { Header } from "../../components/Header/Header";
+
 
 const ErrorPage = () => {
     const { goBack } = useAppNavigate();
     return (
-        <>
-            <h1>ErrorPage</h1>
-            <button onClick={() => { goBack() }} >Voltar</button>
-        </>
+    <>
+        <Header/>
+        <ErrorPageContiner>
+            <StyleTitle>Error 404</StyleTitle>
+            <LogoImg src={Logo} alt="Logo Labenu" />
+            <StyleText>Ops, está página não existe!</StyleText>
+            <StyleButton onClick={() => { goBack() }} >Voltar</StyleButton>
+        </ErrorPageContiner>
+    </>
     );
 }
 
